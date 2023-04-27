bdbd3286ee919f9df4a1d6925078623ebe1d4788
Eric Wang <ericwang@Erics-MacBook-Air.local>
2023-04-27 09:00:49 -0700

    restored error detection

--- test/evaluatorTest.py
+++ test/evaluatorTest.py
@@ -3,15 +3,12 @@ from language.evaluator import Evaluator
 def evaluatorRun(ast):
     print('evaluating')
     output = 'Output: '
-    result = Evaluator(ast).evaluate()
-    if result:
-        output += str(result)
-    # try:
-    #     result = Evaluator(ast).evaluate()
-    #     if result:
-    #         output += str(result[0])
-    # except Exception as e:
-    #     output += 'Error: ' + str(e)
+    try:
+        result = Evaluator(ast).evaluate()
+        if result:
+            output += str(result)
+    except Exception as e:
+        output += 'Error: ' + str(e)
     return output
 
 
