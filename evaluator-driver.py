b1a6a3feb5a53639d984ddc675b04cc70d4d6bc0
Eric Wang <ericwang@dhcp-10-15-163-111.wireless.pacific.edu>
2023-04-27 18:05:36 -0700

    evaluator-driver added error catching back.

--- evaluator-driver.py
+++ evaluator-driver.py
@@ -7,24 +7,23 @@ from test.scannerTest import scannerRun
 
 def run(inputFile):
     line = inputFile.read()
-    print('line', line)
     #input = '3 * (5 + 10 / 3 - 1)'
     #evaluatorRun(input)
     output = ''
-    # try:
-    #     lineOutput, lineTokens= scannerRun(line)
-    #     output += lineOutput + '\n'
-    #     parsedOutput, ast = parserRun(lineTokens, False, True)
-    #     output += 'AST: \n' + parsedOutput + '\n'
-    #     output += evaluatorRun(ast)
-    # except Exception as e:
-    #     print(e)
-    #     output += "\n" + str(e)
-    lineOutput, lineTokens= scannerRun(line)
-    output += lineOutput + '\n'
-    parsedOutput, ast = parserRun(lineTokens, False, True)
-    output += 'AST: \n' + parsedOutput + '\n'
-    output += evaluatorRun(ast)
+    try:
+        lineOutput, lineTokens= scannerRun(line)
+        output += lineOutput + '\n'
+        parsedOutput, ast = parserRun(lineTokens, False, True)
+        output += 'AST: \n' + parsedOutput + '\n'
+        output += evaluatorRun(ast)
+    except Exception as e:
+        print(e)
+        output += "\n" + str(e)
+    # lineOutput, lineTokens= scannerRun(line)
+    # output += lineOutput + '\n'
+    # parsedOutput, ast = parserRun(lineTokens, False, True)
+    # output += 'AST: \n' + parsedOutput + '\n'
+    # output += evaluatorRun(ast)
     return output
 
 
